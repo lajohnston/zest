4de3af8067932ce2176ef7148e9eaf7eccb8a930
lajohnston <lajohnston@live.co.uk>
2015-03-29 21:45:14 +0100

    Console basic

--- smsspec.asm
+++ smsspec.asm
@@ -171,6 +171,21 @@ retn
         ld hl, (smsspec.current_describe_message_addr)
         call smsspec.console.out
  
+        ld hl, (smsspec.current_describe_message_addr)
+        call smsspec.console.out
+
+        ld hl, (smsspec.current_describe_message_addr)
+        call smsspec.console.out
+
+        ld hl, (smsspec.current_describe_message_addr)
+        call smsspec.console.out
+
+        ld hl, (smsspec.current_describe_message_addr)
+        call smsspec.console.out
+
+        ld hl, (smsspec.current_describe_message_addr)
+        call smsspec.console.out
+
         ;smsspec.current_test_message_addr: dw
 
         ; Stop program
