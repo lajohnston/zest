a1b93b118c353a10313c59d8f7530936da4f1f13
lajohnston <lajohnston@live.co.uk>
2016-05-01 15:47:23 +0100

    Update 'it' docblock

--- src/testing/it.asm
+++ src/testing/it.asm
@@ -1,6 +1,6 @@
 /**
- * Specified a new test with a description.
- * Resets the Z80 registers ready for the new test
+ * Initialises a new test.
+ * Resets the Z80 registers and stores the test description in case the test fails
  */
 .macro "it" args message
     smsspec.storeText message, smsspec.current_test_message_addr
