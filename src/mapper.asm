36eafb7fd9b842cb856d87afb877dad2b9d4c2be
Lee Johnston <lajohnston@live.co.uk>
2023-08-20 14:47:13 +0100

    Use Sega mapper (4x16KB slots) for hardware compatibility

--- src/mapper.asm
+++ src/mapper.asm
@@ -3,30 +3,27 @@
 ; These contain fixed address ranges that ROM banks can be mapped into
 ;====
 .define zest.mapper.ZEST_SLOT 0     ; fixed Zest code + assets
-.define zest.mapper.SUITE_SLOT 2    ; where user's code is paged
+.define zest.mapper.SUITE_SLOT 1    ; where user's code is paged
 .define zest.mapper.RAM_SLOT 3
 
-.define zest.mapper.SEGA_HEADER_SIZE 16
-.define zest.mapper.ZEST_SLOT_SIZE $3ff0 - zest.mapper.SEGA_HEADER_SIZE
-.define zest.mapper.SUITE_SLOT_ADDRESS zest.mapper.ZEST_SLOT_SIZE + zest.mapper.SEGA_HEADER_SIZE
-
+; Sega mapper (4x16KB slots)
 .memorymap
-    defaultslot zest.mapper.SUITE_SLOT
+    defaultslot 0
 
-    ; ~16KB - Zest code
-    slotsize zest.mapper.ZEST_SLOT_SIZE
+    ; 16KB - Zest code
+    slotsize $4000
     slot zest.mapper.ZEST_SLOT $0000
 
-    ; 16B - Sega header
-    slotsize zest.mapper.SEGA_HEADER_SIZE
-    slot 1 zest.mapper.ZEST_SLOT_SIZE
+    ; 16KB - Pageable user suite
+    slotsize $4000
+    slot zest.mapper.SUITE_SLOT $4000
 
-    ; 32KB - Pageable user code/tests
-    slotsize $8000  ; 32KB
-    slot zest.mapper.SUITE_SLOT zest.mapper.SUITE_SLOT_ADDRESS
+    ; 16KB - Pageable (not used)
+    slotsize $4000
+    slot 2 $8000
 
-    ; 8KB - RAM
-    slotsize $2000  ; 8KB
+    ; 16KB RAM (8KB + 8KB mirror)
+    slotsize $4000
     slot zest.mapper.RAM_SLOT $c000
 .endme
 
@@ -35,21 +32,16 @@
 ; These can be mapped into the slots above
 ;====
 .define zest.mapper.ZEST_BANK 0
-.define zest.mapper.SUITE_BANK_1 2
-.define zest.mapper.SUITE_BANKS 1
+.define zest.mapper.SUITE_BANK_1 1
 
 .rombankmap
-    bankstotal 2 + zest.mapper.SUITE_BANKS
+    bankstotal 2
 
-    ; Zest bank
-    banksize zest.mapper.ZEST_SLOT_SIZE
+    ; 16KB Zest bank
+    banksize $4000
     banks 1
 
-    ; Sega header bank
-    banksize zest.mapper.SEGA_HEADER_SIZE
+    ; 16KB suite bank
+    banksize $4000
     banks 1
-
-    ; Suite banks
-    banksize $8000
-    banks zest.mapper.SUITE_BANKS
 .endro
