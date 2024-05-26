5e9cb54be696313fbea816302a93c2335c64914f
Lee Johnston <lajohnston@live.co.uk>
2024-05-26 20:36:53 +0100

    Remove zest.assertion.word.define

--- src/assertion/word.asm
+++ src/assertion/word.asm
@@ -7,44 +7,6 @@
 .endst
 
 ;====
-; Defines inline assertion data and sets zest.assertion.word.define.returnValue
-; to the addres
-;
-; @in   expectedValue   the expected word value
-; @in   message         pointer to a assertion failure message, or a custom
-;                       message string
-;
-; @out zest.assertion.word.define.returnValue    address of the data
-;====
-.macro "zest.assertion.word.define" isolated args expectedValue message
-    ; Assert arguments (assemble-time)
-    zest.utils.validate.equals NARGS 2 "\.: Unexpected number of arguments"
-    zest.utils.validate.word expectedValue "\. expectedValue should be a 16-bit value"
-
-    ; Define data
-    .if \?2 == ARG_STRING
-        jp +
-            \.\@_assertionData:
-                .dw expectedValue
-                .dw _customMessage
-                _customMessage:
-                    zest.console.defineString message
-        +:
-    .elif \?2 == ARG_LABEL
-        jr +
-            \.\@_assertionData:
-                .dw expectedValue
-                .db <(message)
-                .db >(message)
-        +:
-    .else
-        zest.utils.validate.fail "\.: message should be a string or a label"
-    .endif
-
-    .redefine zest.assertion.word.define.returnValue (\.\@_assertionData)
-.endm
-
-;====
 ; Generates a call the given routine followed immediately by the assertion data.
 ; The routine should use ex (sp), hl to pop the assertion data pointer from the
 ; stack.
